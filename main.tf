69c0ad2cdb822ee312ff20f32f6b953f8f2a826d
Vimal Paliwal <vimalpaliwal.me@gmail.com>
2021-07-13 11:00:47 +0530

    restrict default security group

--- main.tf
+++ main.tf
@@ -24,20 +24,6 @@ resource "aws_vpc_ipv4_cidr_block_association" "this" {
 
 resource "aws_default_security_group" "this" {
   vpc_id = aws_vpc.this.id
-
-  ingress {
-    protocol  = -1
-    self      = true
-    from_port = 0
-    to_port   = 0
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
 
 resource "aws_default_network_acl" "this" {
